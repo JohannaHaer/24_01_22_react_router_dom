ae77e419b6dac10736bf6ef909512d46bbf58805
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-22 14:41:31 +0100

    fertig

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,29 +5,21 @@ const Nav = ({bgSpeise, bgKontakt, bgOeffnungsz, bgGallerie}) => {
     return (
         <>
             <nav>
-                {bgSpeise ? (
-                    <Link className="active" to='/'>Speisekarte</Link>
-                ) : (
-                    <Link to='/'>Speisekarte</Link>
-                )
+                {bgSpeise 
+                    ? ( <Link className="active" to='/'>Speisekarte</Link>)
+                    : ( <Link to='/'>Speisekarte</Link>)
                 }
-                {bgKontakt ? (
-                    <Link className="active" to='/kontakt'>Kontakt</Link>
-                ) : (
-                    <Link to='/kontakt'>Kontakt</Link>
-                )
+                {bgKontakt 
+                    ? ( <Link className="active" to='/kontakt'>Kontakt</Link>)
+                    : ( <Link to='/kontakt'>Kontakt</Link>)
                 }
-                {bgOeffnungsz ? (
-                    <Link className="active" to='/oeffnungszeiten'>Öffnungszeiten</Link>
-                ) : (
-                    <Link to='/oeffnungszeiten'>Öffnungszeiten</Link>
-                )
+                {bgOeffnungsz 
+                    ? ( <Link className="active" to='/oeffnungszeiten'>Öffnungszeiten</Link>)
+                    : ( <Link to='/oeffnungszeiten'>Öffnungszeiten</Link>)
                 }
-                {bgGallerie ? (
-                    <Link className="active" to='/gallerie'>Gallerie</Link>
-                ) : (
-                    <Link to='/gallerie'>Gallerie</Link>
-                )
+                {bgGallerie 
+                    ? ( <Link className="active" to='/gallerie'>Gallerie</Link>)
+                    : ( <Link to='/gallerie'>Gallerie</Link>)
                 }
             </nav>
         </>
